d71ed586ee067865f88886ba3da323ba8d4245a5
andrew-lenets <andrew.lenets@gmail.com>
2015-02-21 18:12:07 +0200

    add user repository and service

---
+++ src/main/java/ua/dou/hack/service/UserService.java
@@ -0,0 +1,10 @@
+package ua.dou.hack.service;
+
+import ua.dou.hack.domain.User;
+import ua.dou.hack.repository.common.Operations;
+
+/**
+ * mocker on 21.02.15 at 18:08.
+ */
+public interface UserService extends Operations<User, Integer> {
+}
