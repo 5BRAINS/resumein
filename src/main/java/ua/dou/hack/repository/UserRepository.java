b2bc0c96b37f99c13923d378896509292b8d876e
Dmytro Troshchuk <shtorm308@gmail.com>
2015-02-21 22:42:35 +0200

    Implement read user by token

--- src/main/java/ua/dou/hack/repository/UserRepository.java
+++ src/main/java/ua/dou/hack/repository/UserRepository.java
@@ -7,4 +7,5 @@ import ua.dou.hack.repository.common.GenericRepository;
  * mocker on 21.02.15 at 17:53.
  */
 public interface UserRepository extends GenericRepository <User, Integer> {
+    User readByToken(String token);
 }
--- src/main/java/ua/dou/hack/repository/hibernateImpl/HibernateUserRepository.java
+++ src/main/java/ua/dou/hack/repository/hibernateImpl/HibernateUserRepository.java
@@ -1,5 +1,6 @@
 package ua.dou.hack.repository.hibernateImpl;
 
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import ua.dou.hack.domain.User;
 import ua.dou.hack.repository.UserRepository;
@@ -9,8 +10,17 @@ import ua.dou.hack.repository.common.GenericRepositoryHibernateImpl;
  * mocker on 21.02.15 at 18:05.
  */
 @Repository
-public class HibernateUserRepository extends GenericRepositoryHibernateImpl<User, Integer> implements UserRepository {
+public class HibernateUserRepository
+        extends GenericRepositoryHibernateImpl<User, Integer>
+        implements UserRepository {
     public HibernateUserRepository() {
         setType(User.class);
+    }
+
+    @Override
+    public User readByToken(String token) {
+        return (User) getCurrentSession().createCriteria(User.class)
+                                         .add(Restrictions.eq("token", token))
+                                         .uniqueResult();
     }
 }
