815466c4be009624ed34158ce44a571630959943
andrew-lenets <andrew.lenets@gmail.com>
2015-02-22 02:40:19 +0200

    add grabUserInfo method

--- src/main/java/ua/dou/hack/service/ResumeService.java
+++ src/main/java/ua/dou/hack/service/ResumeService.java
@@ -1,10 +1,12 @@
 package ua.dou.hack.service;
 
 import ua.dou.hack.domain.Resume;
+import ua.dou.hack.domain.User;
 import ua.dou.hack.repository.common.Operations;
 
 /**
  * mocker on 22.02.15 at 2:20.
  */
 public interface ResumeService extends Operations<Resume, Integer> {
+    String grabUserInfo(User user);
 }
--- src/main/java/ua/dou/hack/service/impl/ResumeServiceImpl.java
+++ src/main/java/ua/dou/hack/service/impl/ResumeServiceImpl.java
@@ -1,28 +1,50 @@
 package ua.dou.hack.service.impl;
 
+import org.apache.http.client.methods.HttpGet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.dou.hack.domain.Resume;
+import ua.dou.hack.domain.User;
 import ua.dou.hack.repository.ResumeRepository;
 import ua.dou.hack.repository.common.Operations;
 import ua.dou.hack.service.ResumeService;
 import ua.dou.hack.service.common.AbstractService;
+import ua.dou.hack.utils.ResponseUtils;
 
 /**
  * mocker on 22.02.15 at 2:23.
  */
 @Service
 @Transactional
+@PropertySource("classpath:userInfo.properties")
 public class ResumeServiceImpl
         extends AbstractService<Resume, Integer>
         implements ResumeService {
 
     @Autowired
+    private Environment env;
+    
+    @Autowired
     private ResumeRepository resumeRepository;
 
+    @Autowired
+    private ResponseUtils responseUtils;
+    
     @Override
     protected Operations<Resume, Integer> getRepository() {
         return resumeRepository;
+    }
+
+    @Override
+    public String grabUserInfo(User user) {
+        String url = env.getProperty("user.info.link");
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.addHeader("Connection", "Keep-Alive");
+        httpGet.addHeader("Authorization", "Bearer " + user.getToken());
+        String entity = responseUtils.getEntity(httpGet);
+        return entity;
     }
 }
